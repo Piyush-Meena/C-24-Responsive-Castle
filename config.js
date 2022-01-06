60cdbcba24425d04c75b89b41b099af4e5077fab
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2022-01-06 13:39:07 +0530

    Config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "tHLIzaSyALyzx2Xux0hb9y9EGAOHBKgm5nb-6N4",
+    authDomain: "#private",
+    projectId: "#private",
+    storageBucket: "private",
+    messagingSenderId: "567645654897",
+    appId: "1:56635526122:web:9c076a091a8d1ee1a25185",
+    measurementId: "G-67DGVLCTPQ"
+};
+© 2022 GitHub,
